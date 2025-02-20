d1992b84b4af12328fe74c5e8e6cee8a105cbe27
rkdtmdals7710 <143632658+rkdtmdals7710@users.noreply.github.com>
2025-02-20 09:47:38 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int solution(int n){   
+    int answer = 0;
+    string s = to_string(n);
+    
+    for(auto v : s)
+        answer += (v - '0');
+    
+    return answer;
+}
