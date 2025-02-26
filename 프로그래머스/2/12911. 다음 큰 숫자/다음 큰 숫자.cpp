eba4e834bf26eb199946a038deb81deb99ad19b5
rkdtmdals7710 <143632658+rkdtmdals7710@users.noreply.github.com>
2025-02-26 10:17:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+int countOne(string s){
+    int cnt = 0;
+    
+    for(auto v : s)
+        if( v == '1')
+            cnt ++;
+    
+    return cnt;
+}
+
+int solution(int n) {
+    bitset<8> binary(n);
+    int cnt = countOne(binary.to_string());
+    
+    while(++n){
+        bitset<8> tmp(n);
+        if(cnt == countOne(tmp.to_string()))
+            return n;
+    }
+    
+    return -1;
+}
